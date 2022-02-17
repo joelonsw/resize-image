ca10048806508031f94fb68719d4787fb6cc5b78
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2022-02-17 23:09:42 +0900

    feat: enable scheduling

--- src/main/java/com/example/resizeimage/ResizeImageApplication.java
+++ src/main/java/com/example/resizeimage/ResizeImageApplication.java
@@ -2,8 +2,10 @@ package com.example.resizeimage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ResizeImageApplication {
 
 	public static void main(String[] args) {
