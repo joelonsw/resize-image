37da488acf1cc83dc4fe11d237fb658e33e38149
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2022-02-18 16:51:27 +0900

    feat: logging image saving logic

--- src/main/java/com/example/resizeimage/image/repository/ImageRepository.java
+++ src/main/java/com/example/resizeimage/image/repository/ImageRepository.java
@@ -1,5 +1,6 @@
 package com.example.resizeimage.image.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
@@ -9,19 +10,26 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 @Repository
+@Slf4j
 public class ImageRepository {
 
     @Value("${image.path}")
     private String path;
 
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
     public void save(String fileName, String extension, BufferedImage bufferedImage) {
         File fileToSave = new File(path + fileName);
         try {
             ImageIO.write(bufferedImage, extension, fileToSave);
+            LocalDateTime now = LocalDateTime.now();
+            log.info(fileToSave.getName() + " saved at " + dateTimeFormatter.format(now));
         } catch (IOException e) {
             throw new IllegalStateException("File Save Failed");
         }
@@ -46,6 +54,8 @@ public class ImageRepository {
             long fileAge = new Date().getTime() - file.lastModified();
             long desiredAge = TimeUnit.DAYS.toMillis(days);
             if (fileAge > desiredAge) {
+                LocalDateTime now = LocalDateTime.now();
+                log.info(file.getName() + " deleted at " + dateTimeFormatter.format(now));
                 file.delete();
             }
         }
